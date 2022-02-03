b0dabb1a9d247facf34e71a7cdcf302579c389be
Mohd-Tauhid-au26 <tauhidsaifi26748@gmail.com>
2022-02-03 22:36:52 +0530

    created many endpoints like addnote deletenote etc

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,14 @@
 const connectToMongo = require('./db');
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 5000
 
 connectToMongo();
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello Tohid!')
-})
+//Available Routes
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/notes', require('./routes/notes'));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}/`)
