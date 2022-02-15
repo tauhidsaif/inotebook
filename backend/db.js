66602826c678fca61ee6227b5cd9a564782f09ff
Mohd-Tauhid-au26 <tauhidsaifi26748@gmail.com>
2022-02-15 20:55:03 +0530

    some editing

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const mongoURI = "mongodb+srv://tauhidsaif26748:Mohd0000@inotebook.jbrgg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
  const connectToMongo =()=>{
-     mongoose.connect(mongoURI, ()=>{
+     mongoose.connect(mongoURI,{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false}, ()=>{
          console.log("connected to mongo successfully")
      })
  }
