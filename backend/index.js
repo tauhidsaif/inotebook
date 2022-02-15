375d64190f2bf404dfc8573e1e7003dcfbb62f8a
Mohd-Tauhid-au26 <tauhidsaifi26748@gmail.com>
2022-02-15 15:26:29 +0530

    for herouku

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 const connectToMongo = require('./db');
 const express = require('express')
 const app = express()
+const path = require("path");
+
 const port = process.env.PORT || 5000;
 var cors = require('Cors')
 app.use(cors())
@@ -13,6 +15,13 @@ app.use(express.json());
 //Available Routes
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/notes', require('./routes/notes'));
+
+// Step 1:
+app.use(express.static(path.resolve(__dirname, "../build")));
+// Step 2:
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, '../build', "index.html"));
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}/`)
