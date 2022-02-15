956ba1e2744a341f96fb490aa4d28b2cdada565e
Mohd-Tauhid-au26 <tauhidsaifi26748@gmail.com>
2022-02-15 10:58:32 +0530

    added processenv or mongo uri

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const connectToMongo = require('./db');
 const express = require('express')
 const app = express()
-const port = 5000;
+const port = PROCESS.ENV.PORT || 5000;
 var cors = require('Cors')
 app.use(cors())
 app.use(express.json())
