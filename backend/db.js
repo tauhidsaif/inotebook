8474fb666802212254c525d56afcaaf37526398c
Mohd-Tauhid-au26 <tauhidsaifi26748@gmail.com>
2022-02-15 12:01:44 +0530

    mongo atlas working

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const mongoURI = "mongodb+srv://tauhidsaif26748:<password>@inotebook.jbrgg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const mongoURI = "mongodb+srv://tauhidsaif26748:Mohd0000@inotebook.jbrgg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
  const connectToMongo =()=>{
      mongoose.connect(mongoURI, ()=>{
          console.log("connected to mongo successfully")
--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const connectToMongo = require('./db');
 const express = require('express')
 const app = express()
-const port = PROCESS.ENV.PORT || 5000;
+const port = process.env.PORT || 5000;
 var cors = require('Cors')
 app.use(cors())
 app.use(express.json())
