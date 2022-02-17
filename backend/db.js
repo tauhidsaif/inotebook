26b45fb44d2a79bb63e00970f2136c6ba089785a
Mohd-Tauhid-au26 <tauhidsaifi26748@gmail.com>
2022-02-17 10:11:16 +0530

    did some changes

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const mongoURI = "mongodb+srv://tauhidsaif26748:Mohd0000@inotebook.jbrgg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
  const connectToMongo =()=>{
-     mongoose.connect(process.env.MONGODB_URI || mongoURI,{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false}, ()=>{
+     mongoose.connect(process.env.MONGODB_URL || mongoURI,{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false}, ()=>{
          console.log("connected to mongo successfully")
      })
  }
