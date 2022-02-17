2b0f6336bda7795b59d08b0e476e91ddd5ae259e
Mohd-Tauhid-au26 <tauhidsaifi26748@gmail.com>
2022-02-17 20:05:55 +0530

    added again

--- backend/server.js
+++ backend/server.js
@@ -19,12 +19,12 @@ app.use(express.json());
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/notes', require('./routes/notes'));
 
-// // Step 1:
-// app.use(express.static(path.resolve(__dirname, "../build")));
-// // Step 2:
-// app.get("*", function (request, response) {
-//   response.sendFile(path.resolve(__dirname, "../build", "index.html"));
-// });
+// Step 1:
+app.use(express.static(path.resolve(__dirname, "../build")));
+// Step 2:
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "../build", "index.html"));
+});
 
 app.listen(process.env.PORT || port, () => {
   console.log(`Example app listening at http://localhost:${port}/`)
