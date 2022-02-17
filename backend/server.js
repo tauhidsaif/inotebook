64357e9a210928afdf7c624f9bdbc0a4a0573f0f
Mohd-Tauhid-au26 <tauhidsaifi26748@gmail.com>
2022-02-17 19:12:26 +0530

    edited index.js

--- backend/server.js
+++ backend/server.js
@@ -19,10 +19,12 @@ app.use(express.json());
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/notes', require('./routes/notes'));
 
-app.use(express.static(path.join(__dirname, '../build')))
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './index.html'))
-})
+// Step 1:
+app.use(express.static(path.resolve(__dirname, "../build")));
+// Step 2:
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "../build", "index.html"));
+});
 
 app.listen(process.env.PORT || port, () => {
   console.log(`Example app listening at http://localhost:${port}/`)
