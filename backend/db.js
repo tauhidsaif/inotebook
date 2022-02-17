6a07c35e93de973caa120a10a2541ea1ba99e8bf
Mohd-Tauhid-au26 <tauhidsaifi26748@gmail.com>
2022-02-17 11:03:11 +0530

    removed parsers

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
 const mongoURI = "mongodb+srv://tauhidsaif26748:Mohd0000@inotebook.jbrgg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+// const mongoURI = process.env.MONGODB_URL
  const connectToMongo =()=>{
-     mongoose.connect(process.env.MONGODB_URL || mongoURI,{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false}, ()=>{
+     mongoose.connect( mongoURI,{ useNewUrlParser: true}, ()=>{
          console.log("connected to mongo successfully")
      })
  }
