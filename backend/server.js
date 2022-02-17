272b822d99ae0019fefcbb3d2de3627c6d8ccd80
Mohd-Tauhid-au26 <tauhidsaifi26748@gmail.com>
2022-02-17 12:19:27 +0530

    added cors

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const bodyParser = require('body-parser');
 const app = express()
 const path = require("path");
-const cors = require('Cors')
+const cors = require('cors')
 
 
 const port =  5000;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import cors from 'cors'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+
+app.use(cors())
 
 ReactDOM.render(
   <React.StrictMode>
